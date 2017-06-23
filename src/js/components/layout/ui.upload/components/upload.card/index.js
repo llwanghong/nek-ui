5a96b54827e242c215c9eedc83ebba9233b5e57d
hzwanghong <hzwanghong@corp.netease.com>
2017-06-23 11:28:12 +0800

    misc: 删除多余接口参数

--- src/js/components/layout/ui.upload/components/upload.card/index.js
+++ src/js/components/layout/ui.upload/components/upload.card/index.js
@@ -9,42 +9,17 @@ var _ = require('../../../../../ui-base/_');
 var FileUnit = require('../file.unit');
 var UploadBase = require('../upload.base');
 var ImagePreview = require('../image.preview');
-var Config = require('../../config');
 var tpl = require('./index.html');
 
 /**
  * @class UploadCard
- * @extend Component
- * @param {object}         [options.data]                  = 绑定属性
- * @param {string}         [options.data.action]           => 必选，上传地址
- * @param {string}         [options.data.name]             => 可选，上传的文件字段名
- * @param {boolean}        [options.data.multiple]         => 可选，是否支持多选
- * @param {boolean}        [options.data.drag]             => 可选，是否支持拖拽上传
- * @param {string}         [options.data.accept]           => 可选，接受上传的文件类型
- * @param {string}         [options.data.listType]         => 可选，上传组件的展示形式
- * @param {number}         [options.data.numLimit]         => 可选，最大允许上传文件的个数
- * @param {number}         [options.data.numPerline]       => 可选，每行展示的文件个数
- * @param {string}         [options.data.maxSize]          => 可选，上传文件大小的最大允许值
+ * @extend UploadBase
  */
 
 var UploadCard = UploadBase.extend({
     name: 'upload.card',
     template: tpl.replace(/([>}])\s*([<{])/g, '$1$2'),
     config: function(data) {
-        _.extend(data, {
-            action: '',
-            name: 'file',
-            multiple: false,
-            drag: false,
-            accept: '*',
-            listType: 'list',
-            data: {},
-            numLimit: 10,
-            numPerline: 5,
-            maxSize: Config.sizeMap.GB,
-            encType: 'multipart/form-data'
-        });
-
         _.extend(data, {
             status: 'uploaded',
             info: '',
